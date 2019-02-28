19c45f159ad8cce72f44812ac8b6035b863e7a4e
jumormt <jumormt@163.com>
2019-02-28 17:41:13 +0800

    [add] operating in ubtuntu

--- getSourceCFG/test.py
+++ getSourceCFG/test.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 
-cmd = "clang -cc1 -analyze -analyzer-checker=debug.DumpCFG test.c -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+# cmd = "clang -cc1 -analyze -analyzer-checker=debug.DumpCFG test.c -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+cmd = "../tool ../tests/traverse.c"
 res = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=True)
 result = res.stderr.readlines()
 for i in result:
